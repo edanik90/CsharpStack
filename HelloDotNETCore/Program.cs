26d3a390f4f7d460cebcbb4e5be3cc44d718f82c
Dan <e.danik90@gmail.com>
2020-02-02 15:02:46 -0600

    initial commit

---
+++ HelloDotNETCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDotNETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет!");
+        }
+    }
+}
