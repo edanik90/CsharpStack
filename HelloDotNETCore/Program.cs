98c989573a56e19ac1be2a4e66b5e31be9b9d316
Dan <e.danik90@gmail.com>
2020-02-02 15:06:10 -0600

    first assignment added/updated

--- HelloDotNETCore/Program.cs
+++ HelloDotNETCore/Program.cs
@@ -6,7 +6,7 @@ namespace HelloDotNETCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет!");
+            Console.WriteLine("Yellow, world!");
         }
     }
 }
