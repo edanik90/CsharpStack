535a3a31e69cf42c54da5cff3faab19dc4c2d185
Dan <e.danik90@gmail.com>
2020-02-03 12:40:21 -0600

    CollectionsPractice added

---
+++ LanguageEssentials/CollectionsPractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Three Basic Arrays
+            int[] FirstArray = {0,1,2,3,4,5,6,7,8,9};
+            string[] SecondArray = {"Tim", "Martin", "Nikki", "Sara"};
+            bool[] ThirdArray = {true, false, true, false, true, false, true, false, true, false};
+            Console.WriteLine(ThirdArray.Length);
+
+            // List of Favors
+            List<string> IceCreamFLavors = new List<string>();
+            IceCreamFLavors.Add("Chocolate");
+            IceCreamFLavors.Add("Vanilla");
+            IceCreamFLavors.Add("Banana");
+            IceCreamFLavors.Add("Strawberry");
+            IceCreamFLavors.Add("Mango");
+            Console.WriteLine(IceCreamFLavors.Count);
+            Console.WriteLine(IceCreamFLavors[2]);
+            IceCreamFLavors.RemoveAt(2);
+            Console.WriteLine(IceCreamFLavors.Count);
+
+            // User Info Dictionary
+            Dictionary<string,string> MyDictionary = new Dictionary<string,string>();
+            for (int i = 0; i < SecondArray.Length; i++)
+            {
+                MyDictionary.Add(SecondArray[i], IceCreamFLavors[i]);
+            }
+            foreach (KeyValuePair<string,string> entry in MyDictionary)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+        }
+    }
+}
