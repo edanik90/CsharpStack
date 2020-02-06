d558e9039e2a84ec5b991f6c66c9225c65959e74
Dan <e.danik90@gmail.com>
2020-02-06 16:26:38 -0600

    Multiplications Table added

--- LanguageEssentials/MultuplicationTable/Program.cs
+++ LanguageEssentials/MultuplicationTable/Program.cs
@@ -6,7 +6,16 @@ namespace MultuplicationTable
     {
         static void Main(string[] args)
         {
-            int[,] array2D = new int[10,10];
+            int[,] array2D = new int[10, 10];
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    array2D[i-1,j-1] = i * j;
+                    Console.Write($"{array2D[i-1,j-1]} ");
+                }
+            Console.WriteLine("");
+            }
         }
     }
 }
