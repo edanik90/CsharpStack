aa6ca562983738a4343ca900e09804244ad9b6cd
Dan <e.danik90@gmail.com>
2020-02-10 12:07:38 -0600

    DojoSurvey with model added

---
+++ ASPdotNET/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("process")]
+        public IActionResult Success(Dojo newDojo)
+        {
+            ViewBag.Name = newDojo.Name;
+            ViewBag.Location = newDojo.Location;
+            ViewBag.Language = newDojo.Language;
+            ViewBag.Comment = newDojo.Comment;
+            return View("Success");
+        }
+    }
+}
