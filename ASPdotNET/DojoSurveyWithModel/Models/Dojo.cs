9cbe23da1f597c2ae48905b4c9cc8d004bd35af9
Dan <e.danik90@gmail.com>
2020-02-10 17:18:55 -0600

    DojoSurveyWithModels updated/validations added

--- ASPdotNET/DojoSurveyWithModel/Models/Dojo.cs
+++ ASPdotNET/DojoSurveyWithModel/Models/Dojo.cs
@@ -3,17 +3,17 @@ namespace DojoSurveyWithModel.Models
 {
     public class Dojo
     {
-        [Required]
-        [MinLength(2)]
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(2, ErrorMessage = "Name should be at least 2 characters long.")]
         public string Name {get;set;}
 
-        [Required]
+        [Required(ErrorMessage = "Location is required")]
         public string Location {get;set;}
 
-        [Required]
+        [Required(ErrorMessage = "Language is required")]
         public string Language {get;set;}
 
-        [MaxLength(20)]
+        [MaxLength(20, ErrorMessage = "Comment cannot be more than 20 characters")]
         public string Comment {get;set;}
     }
 }
